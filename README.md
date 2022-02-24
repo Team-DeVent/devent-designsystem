1f5c8414f21e35bc5f46732459f6906e20f3e44f
Dipokal HHJ <hhj@devent.kr>
2022-02-24 14:02:19 +0900

    feat: navbar
    + Navbar 디자인 적용

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
+![ds](./head.png)
 
-# devent-designsystem
+# devent-designsystem-prototype
 디자인 시스템 프로토타입
 
 ## To-Do
 
-* [ ] navbar
+* [x] navbar
+* [ ] header
+* [ ] footer
+* [ ] 커스텀 엘리먼트 번들 파일 생성
 
+## 목표
+
+데벤트가 배포하는 서비스마다 디자인의 통일감을 준다.  
+같은 코드의 반복을 낮춰 효율적인 개발을 가능하게 한다.  
+최종적으로 CDN 서버에 배포하며 하나의 디자인만 변경해도 동시에 적용되게끔 한다.  
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,67 +1,94 @@
 class Navbar extends HTMLElement { 
+    constructor() {
+        super()
+        this.addEventListener('click', e => {
+            this.buttonToggle()
+        });
+    }
+
+    buttonToggle() {
+        let is_expanded = document.querySelector("#navbar_toggle_button").getAttribute("aria-expanded")
+        let icon = document.querySelector("#navbar_toggle_icon")
+
+        if (is_expanded == 'true') {
+            icon.classList.add('fa-times')
+            icon.classList.remove('fa-bars')
+        } else {
+            icon.classList.add('fa-bars')
+            icon.classList.remove('fa-times')
+        }
+    }
+
     connectedCallback() {
-        let title = this.getAttribute('nav-title');
+        let title = this.getAttribute('nav-title')
 
         
-        let body_nav = document.createElement('nav');
+        let body_nav = document.createElement('nav')
+        let body_container = document.createElement('div')
+        let body_collapse = document.createElement('div')
+        let collapse_list = document.createElement('ul')
 
-        let body_container = document.createElement('div');
-
-        let body_collapse = document.createElement('div');
-
-        let a_title = document.createElement('a');
-        let button_toggle = document.createElement('button');
-        let button_icon = document.createElement('i');
+        let a_title = document.createElement('a')
+        let button_toggle = document.createElement('button')
+        let button_icon = document.createElement('i')
 
 
-        body_nav.classList.add('navbar', 'navbar-expand-lg', 'navbar-dark', 'navbar-blur', 'fixed-top');
-
-        body_container.classList.add('container-fluid', 'px-0');
+        body_nav.classList.add('navbar', 'navbar-expand-lg', 'navbar-light', 'navbar-blur', 'fixed-top');
+        body_container.classList.add('container-fluid', 'px-0')
 
         a_title.classList.add('navbar-brand')
         a_title.setAttribute('href', '/')
+        a_title.innerText = title
 
         button_toggle.classList.add('navbar-toggler')
+        button_toggle.setAttribute('id', 'navbar_toggle_button')
         button_toggle.setAttribute('type', 'button')
         button_toggle.setAttribute('data-bs-toggle', 'collapse')
-        button_toggle.setAttribute('data-bs-target', '#navbarSupportedContent')
-        button_toggle.setAttribute('aria-controls', 'navbarSupportedContent')
+        button_toggle.setAttribute('data-bs-target', '#navbar_supported_content')
+        button_toggle.setAttribute('aria-controls', 'navbar_supported_content')
         button_toggle.setAttribute('aria-expanded', 'false')
 
         button_icon.classList.add('fas', 'fa-bars')
-
+        button_icon.setAttribute('id', 'navbar_toggle_icon')
 
         body_collapse.classList.add('collapse', 'navbar-collapse')
-        body_collapse.setAttribute('id', 'navbarSupportedContent')
+        body_collapse.setAttribute('id', 'navbar_supported_content')
+        collapse_list.classList.add('navbar-nav', 'ms-auto', 'mb-2', 'mb-lg-0')
+        collapse_list.setAttribute('id', 'navbar_collapse')
 
 
-        
-
-        profileImage.src = this.getAttribute('profile-src');
-        profileImage.width = '40';
-        profileImage.onclick = function() { location.href=`/@${uid}` }
-
-        this.appendChild(body_nav);
-
+        button_toggle.appendChild(button_icon)
+        body_container.appendChild(a_title)
+        body_container.appendChild(button_toggle)
+        body_container.appendChild(body_collapse)
+        body_collapse.appendChild(collapse_list) 
+        body_nav.appendChild(body_container)
+        this.appendChild(body_nav)
     }
-    
 }
-    
-customElements.define('navbar', Navbar);
 
-<nav class="navbar navbar-expand-lg navbar-dark navbar-blur fixed-top">
-    <div class="container px-5">
-        <a class="navbar-brand" href="#!">Frame</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+class NavbarItem extends HTMLElement { 
+    connectedCallback() {
+        let title = this.getAttribute('item-title')
+        let link = this.getAttribute('item-link')
+        
+        let body_list = document.querySelector("#navbar_collapse")
+        let item_li = document.createElement('li')
+        let item_link = document.createElement('a')
+
+        item_li.classList.add('navbar-item');
+        item_link.classList.add('nav-link')
+        item_link.setAttribute('aria-current', 'page')
+        item_link.setAttribute('href', link)
+        item_link.innerText = title
+
+        item_li.appendChild(item_link)
+        body_list.appendChild(item_li)
+    }
+}
+
+    
+customElements.define('dds-navbar', Navbar);
+customElements.define('dds-navbar-item', NavbarItem);
+
+
